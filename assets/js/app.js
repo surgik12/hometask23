f0a4cd01d4908c6e80b135c528b80ac3aa030bf7
surgik12 <127215052+surgik12@users.noreply.github.com>
2023-04-24 19:50:02 +0300

    1

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+let tryCount = 10;
+
+let variant = 1 + Math.floor(Math.random() * 100);
+
+let endGame = false;
+
+function start(){
+    if (endGame == true){
+        return;
+    }
+
+    let number = +userNumber.value;
+
+    if(number > 100){
+        number = 100;
+    }
+
+    if (tryCount == 0){
+        compNumber.innerHTML = `Игра окончена! ${variant}`;
+        return;
+    } else {
+        userVariant.innerHTML = `${number}`;
+    }
+
+    tryCount--;
+    console.log(tryCount);
+
+    if (variant > number){
+        compNumber.innerHTML = `Ваше число меньше загаданного.`;
+    } else if(variant < number){
+        compNumber.innerHTML = `Ваше число больше загаданного.`;
+    } else if(variant = number){
+        compNumber.innerHTML = `Поздравляем вы выиграли! Это было ${variant}`;
+        endGame = true;
+    }
+
+    userTry.innerHTML = `${tryCount}`;
+    console.log(variant, number);
+}
